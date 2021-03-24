cea3dbfc766b9d7597b49013a61943cc18375979
Rabire <rabireh@outlook.fr>
2021-03-24 13:34:25 +0100

    dynamic file yessssss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import "./App.css";
 import pastelColors from "./colors";
 
-import img from "./assets/yann-1.png";
-import img3 from "./assets/yann-3.png";
-import img4 from "./assets/yann-4.png";
+// import img from "./assets/yann-1.png";
+// import img3 from "./assets/yann-3.png";
+// import img4 from "./assets/yann-4.png";
 
 const randomColor =
   pastelColors[Math.floor(Math.random() * pastelColors.length)];
 
 const friends = ["noam", "yann"];
 const randomFriend = friends[Math.floor(Math.random() * friends.length)];
-console.log({ randomFriend });
 
 function App() {
+  const iconsPath = require.context("./assets/", true);
+
+  const img1 = iconsPath("./" + randomFriend + "-1.png").default;
+  const img3 = iconsPath("./" + randomFriend + "-3.png").default;
+  const img4 = iconsPath("./" + randomFriend + "-4.png").default;
+
   return (
     <div
       id="snow"
       style={{
         backgroundColor: randomColor,
-        backgroundImage: `url(${img}), url(${img3}), url(${img4})`,
+        backgroundImage: `url(${img1}), url(${img3}), url(${img4})`,
       }}
     ></div>
   );
