8cfcc373f7a95d4ee57eebcd61da9542ba8b6c39
Rabire <rabireh@outlook.fr>
2021-03-24 13:02:10 +0100

    assets & content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
+import pastelColors from "./colors";
+
+import img from "./assets/yann-1.png";
+import img3 from "./assets/yann-3.png";
+import img4 from "./assets/yann-4.png";
+
+const randomColor =
+  pastelColors[Math.floor(Math.random() * pastelColors.length)];
+
+const friends = ["noam", "yann"];
+const randomFriend = friends[Math.floor(Math.random() * friends.length)];
+console.log({ randomFriend });
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>NTM</p>
-      </header>
-    </div>
+    <div
+      id="snow"
+      style={{
+        backgroundColor: randomColor,
+        backgroundImage: `url(${img}), url(${img3}), url(${img4})`,
+      }}
+    ></div>
   );
 }
 
